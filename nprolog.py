5ad0c45d52d443a2fbb2b2a679ae778d41c76100
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-02-15 22:55:18 -0800

    Read from file and user input

--- nprolog.py
+++ nprolog.py
@@ -2,8 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import string
+import re
+import sys
 
 variables = {}
+
+
+class ParseError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 class Fact:
@@ -20,41 +31,27 @@ class Fact:
 
 class Rule:
 
-	def __init__(self, string):
-		s = string.replace(' ', '').split(':-')
-		self.goal = Fact(s[0])
-		self.subgoals = s[1].split('),')
-		self.dict = {}
+    def __init__(self, string):
+        s = string.replace(' ', '').split(':-')
+        self.goal = Fact(s[0])
+        self.subgoals = s[1].split('),')
+        self.dict = {}
 
-		for i in range(len(self.subgoals)):
-			if self.subgoals[i][-1]!=')':
-				self.subgoals[i]+=')'
+        for i in range(len(self.subgoals)):
+            if self.subgoals[i][-1] != ')':
+                self.subgoals[i] += ')'
 
-		self.subgoals = [Fact(subgoal) for subgoal in self.subgoals]
+        self.subgoals = [Fact(subgoal) for subgoal in self.subgoals]
 
-		self.subgoals_count = len(self.subgoals)
+        self.subgoals_count = len(self.subgoals)
 
-		self.dict[self.goal] = self.subgoals
+        self.dict[self.goal] = self.subgoals
 
-	def __str__(self):
-		return self.goal.functor + '(' + ', '.join(self.goal.args) + ') :- ' + \
-		''.join([subgoal.functor + '(' + ', '.join(subgoal.args) + \
-		'),' for subgoal in self.subgoals])[:-1]
-
-############################################
-#####      TESTING TERMS        ############
-
-rule = Rule('animal(Y,X):-cat(X,Y)')
-
-cat = Fact('cat(tom,john)')
-tom=D
-john=Z
-facts = [cat]
-
-rules = [rule]
-
-
-#############################################
+    def __str__(self):
+        return self.goal.functor + '(' + ', '.join(self.goal.args) \
+            + ') :- ' + ''.join([subgoal.functor + '('
+                                + ', '.join(subgoal.args) + '),'
+                                for subgoal in self.subgoals])[:-1]
 
 
 def unify(fact):
@@ -62,6 +59,8 @@ def unify(fact):
     args = fact.args
     unknowns = fact.unknowns
     response = False
+    global question
+    question = False
 
     if unknowns:
 
@@ -73,37 +72,32 @@ def unify(fact):
                     variables.update(var)
                     response = True
     else:
+
         for f in facts:
-        	if f.functor==fact.functor and f.args==fact.args:
-        		return True
+            if f.functor == fact.functor and f.args == fact.args:
+                question = True
+                return True
 
     return response
 
 
 def search(fact):
-	if unify(fact):
-		return True
-	else:
-		for rule in rules:
-			if fact.functor==rule.goal.functor:
-				for subgoal in rule.subgoals:
+    if unify(fact):
+        return True
+    else:
+        for rule in rulebase:
+            if fact.functor == rule.goal.functor:
+                for subgoal in rule.subgoals:
 
-					if not subgoal.arity==fact.arity:
-						return False
+                    if not subgoal.arity == fact.arity:
+                        return False
 
-					temp = Fact(subgoal.functor+'('+','.join(fact.args)+')')
+                    temp = Fact(subgoal.functor + '('
+                                + ','.join(fact.args) + ')')
 
-					print temp
+                    if unify(temp):
+                        return True
 
-					if unify(temp):
-						return True
-
-
-'''
-Checks if two arrays are equal if we ignore uppercase characters.
-If they are, all uppercase-lowercase pairs are returned.
-
-'''
 
 def equal(arr1, arr2):
 
@@ -122,5 +116,50 @@ def equal(arr1, arr2):
     return match
 
 
-def print_vars():
-	print variables
+def parse(file=sys.argv[1]):
+    f = filter(lambda line: line.strip(), open(file))
+
+    extension = file.split('.')[-1]
+
+    if extension != 'npro':
+        raise ParseError('Cannot parse .' + extension + '-files.')
+
+    for (linenumber, line) in enumerate(f):
+        line = line.replace(' ', '').strip()
+        if ':-' in line:
+            try:
+                rule = Rule(line)
+            except ParseError, e:
+                print "Couldn't parse rule on line: " + str(linenumber)
+            rulebase.append(rule)
+        else:
+
+            try:
+                fact = Fact(line)
+            except ParseError, e:
+                print "Couldn't parse rule on line: " + str(linenumber)
+            facts.append(fact)
+
+
+if __name__ == '__main__':
+    rulebase = []
+    facts = []
+    parse()
+
+    print 'Rules:'
+    for rule in rulebase:
+        print rule
+
+    print '\nFacts:'
+    for fact in facts:
+        print fact
+
+    print '\n'
+    while True:
+
+        prompt = Fact(raw_input('? '))
+
+        search(prompt)
+
+        if question:
+            print 'yes'
