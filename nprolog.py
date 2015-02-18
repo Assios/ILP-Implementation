20bd6a4311f86c19b501d569091d79be6b6d7487
Sondre Basma <sondre1504@gmail.com>
2015-02-18 15:53:25 -0800

    stack method

--- nprolog.py
+++ nprolog.py
@@ -5,7 +5,7 @@ import string
 import re
 import sys
 
-variables = {}
+#variables = {}
 
 
 class ParseError(Exception):
@@ -37,16 +37,23 @@ class Rule:
         self.goal = Fact(s[0])
         self.subgoals = s[1].split('),')
         self.dict = {}
+        self.env = {}
+        self.index = 0
+        self.parent = False
 
         for i in range(len(self.subgoals)):
             if self.subgoals[i][-1] != ')':
                 self.subgoals[i] += ')'
 
         self.subgoals = [Fact(subgoal) for subgoal in self.subgoals]
+        
+        for sub in self.subgoals:
+            sub.parent = self
 
         self.subgoals_count = len(self.subgoals)
 
         self.dict[self.goal] = self.subgoals
+
 
     def __str__(self):
         return self.goal.functor + '(' + ', '.join(self.goal.args) \
@@ -54,8 +61,15 @@ class Rule:
                                 + ', '.join(subgoal.args) + '),'
                                 for subgoal in self.subgoals])[:-1]
 
+    def __eq__(self, other):
+        if isinstance(other, Fact):
+            print "fact in eq: ", other
+            print "rule in eq: ", self
+            return self.goal.functor == other.functor and self.goal.args == other.arity 
+        return (self.goal.functor == other.goal.functor and self.goal.args == other.goal.args)
 
-def unify(fact):
+
+def unify(s_fact, s_env, d_fact, d_env):
     """
     Tries to unify a fact with all the facts in the database.
     Returns False if not possible.
@@ -67,75 +81,116 @@ def unify(fact):
     response = False
 
     if unknowns:
+        if d_fact.functor == s_fact.functor:
 
-        for f in facts:
-            if functor == f.functor:
+            var = equal(d_fact.args, s_fact.args)
 
-                var = equal(args, f.args)
+            if var:
+                d_env.update(var)
 
-                if var:
-                    variables.update(var)
+                for key, value in var.iteritems():
+                    print(key + " = " + value)
 
-                    for key, value in var.iteritems():
-                        print(key + " = " + value)
-
-                    response = True
+                response = True
     else:
-
-        for f in facts:
-            if f.functor == fact.functor and f.args == fact.args:
-                return True
+        if s_fact.functor == d_fact.functor and s_fact.args == d_fact.args:
+            #update environment
+            return True
 
     return response
 
 
-def search(fact):
-    """
-    If it can't unify a fact directly, derive it
-    from the rulebase.
-    """
+# def search(fact):
+#     """
+#     If it can't unify a fact directly, derive it
+#     from the rulebase.
+#     """
 
-    if unify(fact):
-        return True
-    else:
-        for rule in rulebase:
-            if fact.functor == rule.goal.functor:
-                for subgoal in rule.subgoals:
+#     if unify(fact):
+#         return True
+#     else:
+#         for rule in rulebase:
+#             if fact.functor == rule.goal.functor:
+#                 for subgoal in rule.subgoals:
                     
-                    indices = []
+#                     indices = []
 
-                    # SOME TEMPORARY DIRTY HACKS HERE TO MAKE THIS KIND OF STUFF WORK:
-                    # child(X):-mother(Y,X)
+#                     # SOME TEMPORARY DIRTY HACKS HERE TO MAKE THIS KIND OF STUFF WORK:
+#                     # child(X):-mother(Y,X)
 
-                    for i in range(len(rule.goal.args)):
-                        try:
-                            indices.append(subgoal.args.index(rule.goal.args[i]))
-                        except:
-                            continue
+#                     for i in range(len(rule.goal.args)):
+#                         try:
+#                             indices.append(subgoal.args.index(rule.goal.args[i]))
+#                         except:
+#                             continue
 
-                    if indices:
-                        if indices[0]>len(fact.args)-1:
-                            fact.args.insert(0, "Y")
+#                     if indices:
+#                         if indices[0]>len(fact.args)-1:
+#                             fact.args.insert(0, "Y")
 
-                    if "Y" not in fact.args:
-                        fact.args = gen_list(fact.args, indices)
+#                     if "Y" not in fact.args:
+#                         fact.args = gen_list(fact.args, indices)
 
-                    print subgoal.args
-                    print fact.args
+#                     print subgoal.args
+#                     print fact.args
 
-                    if len(fact.args)!=len(subgoal.args):
-                        for element in fact.args:
-                            if element not in subgoal.args:
-                                fact.args.remove(element)
+#                     if len(fact.args)!=len(subgoal.args):
+#                         for element in fact.args:
+#                             if element not in subgoal.args:
+#                                 fact.args.remove(element)
 
-                    temp = Fact(subgoal.functor + '('
-                                + ','.join(fact.args) + ')')
+#                     temp = Fact(subgoal.functor + '('
+#                                 + ','.join(fact.args) + ')')
 
-                    print variables
+#                     print variables
 
-                    if not unify(temp):
-                        return False
+#                     if not unify(temp):
+#                         return False
+#                 return True
+
+
+
+def search(query, env={}):
+    curr_rules = []
+    for rule in rulebase:
+        if rule.goal.functor == query.functor:
+            curr_rules.append(rule)
+    if not len(curr_rules):
+        return False
+    stack = [curr_rules.pop()]
+    while stack:
+        print "in while"
+        rule = stack.pop()
+        if(rule.index < rule.subgoals_count): #we still have more subgoals to compute
+            print "more subgoals"
+            r = rule.subgoals[rule.index]
+
+            for set_rule in rulebase_facts:
+                print "rule: ", set_rule
+                print "r: ", r
+                if set_rule == r:
+                    print "set_rule == r"
+                    if unify(set_rule, set_rule.env, r, r.env):
+                        stack.append(r)
+        else: #subgoals finished, check if there are parents who want to join
+            print "finished subgoals"
+            if(rule.parent):
+                print "finding parent"
+                if(unify(rule, rule.env, rule.parent.subgoals[rule.parent.index], rule.parent.env)):
+                    stack.append(rule.parent)
+                    continue
+            else:
+                print "no parent"
+                print rule.env
                 return True
+
+
+
+
+
+    
+
+
 
 
 def gen_list(array, indices):
@@ -185,13 +240,16 @@ def parse(file=sys.argv[1]):
 
         if ':-' in line:
             rulebase.append(Rule(line))
+            rulebase_facts.append(Rule(line))
         else:
+            rulebase_facts.append(Fact(line))
             facts.append(Fact(line))
 
 
 if __name__ == '__main__':
     rulebase = []
     facts = []
+    rulebase_facts = rulebase + facts
     parse()
 
     print 'Rules:'
